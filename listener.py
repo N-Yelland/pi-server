fbe111ced29b1d3f610f9bb3f2d76a883b4aa573
Nic Yelland <mail@nicyelland.com>
2024-02-03 14:37:50 +0000

    modified listener.py

--- listener.py
+++ listener.py
@@ -5,59 +5,77 @@ import ssl
 import hashlib
 import hmac
 import git
+import logging
 
 from aiohttp import web
 
-SECRET = open("secret_token", "r").read()
-print(SECRET)
+logger = logging.getLogger("WebhookListener")
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("[{asctime}] {name} :: {levelname:<6} :: {message}", style="{")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+SECRET_TOKEN = open("secret_token", "r").read()
 
 
-def verify_signature(payload_body: str, secret_token: str, signature_header: str) -> bool:
+def validate_signature(payload_body: str, secret_token: str, signature_header: str) -> bool:
+    """Function to validate whether a webhook payload has a valid signature. The signature should be the HMAC-SHA256
+    digest of the payload body keyed with a secret token.
+
+    :param payload_body: Body of the POST request
+    :param secret_token: A secret token known by authorised originators
+    :param signature_header: Hex digest of signature in request headers
+    :return: True if valid, otherwise False
+    """
 
     if not signature_header:
-        print("X-Hub-Signature-256 header is missing!")
+        logger.warning("Failed to validate signature: X-Hub-Signature-256 header is missing!")
         return False
     
     hash_object = hmac.new(secret_token.encode("utf8"), msg=payload_body, digestmod=hashlib.sha256)
     expected_signature = "sha256=" + hash_object.hexdigest()
     
     if not hmac.compare_digest(expected_signature, signature_header):
-        print("Failed to validate signature.")
+        logger.warning("Failed to validate signature: signatures do not match!")
         return False
     
-    print("Signature validated, proceding...")
     return True
 
 
-async def run_server():
-    app = web.Application()
+async def webhook_handler(request: web.Request) -> web.Response:
+    """On receipt of a POST request to /webhook, if the signature is valid, this function tries to update the
+    repository. Either way, it will send the appropriate response to the originator.
 
-    routes = web.RouteTableDef()
-
-    @routes.get("/webhook")
-    async def get_handler(request):
-        print("New Connection")
-        return web.Response(text="This is the webhook listener.\n", content_type="text/html")
-    
-    @routes.post("/webhook")
-    async def post_handler(request: web.Request):
-        print("Webhook triggered, recieving payload:")
+    :param request: HTTP POST request to /webhook.
+    :return: HTTP Response (200 if successful, 401 if unauthorised)
+    """
+    logger.info("Webhook triggered: verifying signature.")
         
-        print("Verifying signature...")
-        recieved_signature = request.headers.get("X-Hub-Signature-256")
-        request_body = await request.read()
-        if verify_signature(request_body, SECRET, recieved_signature):
-            print("Pulling repository...")
-            repo = git.Repo("/home/nic/Desktop/pi-server")
-            
-            current = repo.head.commit
-            repo.remotes.origin.pull()
-            if current != repo.head.commit:
-                print("Repository contents have chagned.")
-            print("No change; repository already up-to-date.")
-            
+    recieved_signature = request.headers.get("X-Hub-Signature-256")
+    request_body = await request.read()
+    if validate_signature(request_body, SECRET_TOKEN, recieved_signature):
+        logger.info("Validated signature, pulling repository...")
 
-    app.router.add_routes(routes)
+        repo = git.Repo("/home/nic/Desktop/pi-server")
+        current = repo.head.commit
+        repo.remotes.origin.pull()
+        if current != repo.head.commit:
+            logger.info("Repository contents have chagned.")
+        logger.info("No change; repository already up-to-date.")
+
+        return web.Response(status=200)
+    
+    return web.Response(status=401)
+
+
+
+async def run_server() -> None:
+    app = web.Application()
+    app.add_routes([web.post("/webhook", webhook_handler)])
 
     ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
     ssl_context.load_cert_chain("certs/cert.pem", "certs/cert.key")
@@ -70,42 +88,11 @@ async def run_server():
         ssl_context=ssl_context
     )
     await site.start()
-    print("Webhook listener running...")
+    logger.info("Webhook listener running")
 
     await asyncio.Event().wait()
 
+
 if __name__ == "__main__":
-    print("Starting webhook listener...")
+    logger.info("Starting webhook listener")
     asyncio.run(run_server())
-
-
-"""
-Example webhook payload headers
-
-See https://docs.github.com/en/webhooks/webhook-events-and-payloads
-
-
-<CIMultiDictProxy(
-    'Host': 'pi.nicyelland.com',
-    'Connection': 'Keep-Alive',
-    'Accept-Encoding': 'gzip',
-    'X-Forwarded-For': '140.82.115.38',
-    'CF-RAY': '84cda5f5cf2559d4-IAD',
-    'Content-Length': '7297',
-    'X-Forwarded-Proto': 'https',
-    'CF-Visitor': '{"scheme":"https"}',
-    'User-Agent': 'GitHub-Hookshot/547858f',
-    'Accept': '*/*',
-    'Content-Type': 'application/json',
-    'X-GitHub-Delivery': '798321da-be44-11ee-9341-6160b273cb58',
-    'X-GitHub-Event': 'push',
-    'X-GitHub-Hook-ID': '457504192',
-    'X-GitHub-Hook-Installation-Target-ID': '749487659',
-    'X-GitHub-Hook-Installation-Target-Type': 'repository',
-    'X-Hub-Signature': 'sha1=adfb82aed4868548952c78c040b2c362c41edf1e',
-    'X-Hub-Signature-256': 'sha256=0787c13d36202acd81e20cf847ecaee88a6158f3f25c2f53e0e4ef3b39015f68',
-    'CF-Connecting-IP': '140.82.115.38',
-    'CDN-Loop': 'cloudflare',
-    'CF-IPCountry': 'US'
-)>
-"""
